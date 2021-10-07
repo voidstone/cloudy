f9166af68406a9c54e1476675774713ef3e42d65
Serzh Chaikin <sage@MacBook-Air-Serzh.local>
2021-10-07 12:52:20 +0400

    :recycle: remove fileSize from display

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-const { readFileSync, writeFileSync, statSync } = require('fs')
-const { pipe, prop, __, divide, concat, toString } = require('ramda')
+const { copyFileSync, statSync } = require('fs')
 
 const [INPUT_FILE, OUTPUT_FILE] = process.argv.slice(2);
 
-const getFileSize = pipe(
-    statSync,
-    prop("size"),
-    divide(__, 1024),
-    Math.round,
-    toString,
-    concat(__, 'KB')
-);
 
 try {
-    const fileSize = getFileSize(INPUT_FILE);
-
-    const file = readFileSync(INPUT_FILE, 'utf8')
-
-    writeFileSync(OUTPUT_FILE, file);
+    copyFileSync(INPUT_FILE, OUTPUT_FILE);
 
     if (process.argv.includes('-v')) {
-        console.log(`Copied ${fileSize} from ${INPUT_FILE} -> ${OUTPUT_FILE}`);
+        console.log(` ${INPUT_FILE} -> ${OUTPUT_FILE}`);
     }
 
 } catch (e) {
