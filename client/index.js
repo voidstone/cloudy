9e4e97f0493e09d3006230250f2881c5ddecba66
sage <chaikin.sergey@gmail.com>
2021-10-18 23:41:37 +0400

    :boom: send files to the server

--- client/index.js
+++ client/index.js
@@ -1,24 +1,27 @@
-const { createReadStream, createWriteStream } = require('fs')
+const { createReadStream } = require('fs');
+const { request } = require('http');
 const { createGzip } = require('zlib')
 
 const [INPUT_FILE, OUTPUT_FILE] = process.argv.slice(2);
 
 const readStream = createReadStream(INPUT_FILE);
-const writeStream = createWriteStream(OUTPUT_FILE);
+const writeStream = request("http://localhost:4000/upload", {
+    method: 'POST',
+    headers: { 'file-path': OUTPUT_FILE }
+})
 const compressStream = createGzip();
 
-readStream.pipe(compressStream)
-    .on('data', (chunk) => {
-        writeStream.write(chunk);
-    })
+
+readStream
     .on('end', () => {
         if (process.argv.includes('-v')) {
             console.log(` ${INPUT_FILE} -> ${OUTPUT_FILE}`);
         }
-        writeStream.end()
+        // writeStream.end()
     })
+    .pipe(compressStream)
+    .pipe(writeStream)
     .on('error', (e) => {
         console.error(e.message);
-
         process.exit(1);
     });
